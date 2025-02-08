6a3f76a2427f780027119f1a7a07143708cc9396
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-08 23:28:31 +0100

    i-am-role-eks-nodegroup

--- i-am.tf
+++ i-am.tf
@@ -28,3 +28,19 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks-cluster-role[count.index].name
 }
+
+resource "aws_iam_role" "eks-nodegroup-role" {
+  count = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  name  = "${local.cluster_name}-nodegroup-role-${random_integer.random_suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
