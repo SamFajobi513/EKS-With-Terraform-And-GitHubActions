f811b949ffd3797de5257e3eff517897ed3b35b5
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-08 23:26:16 +0100

    iam policy

--- i-am.tf
+++ i-am.tf
@@ -6,3 +6,19 @@ resource "random_integer" "random_suffix" {
   min = 1000
   max = 9999
 }
+
+resource "aws_iam_role" "eks-cluster-role" {
+  count = var.is_eks_role_enabled ? 1 : 0
+  name  = "${local.cluster_name}-role-${random_integer.random_suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
