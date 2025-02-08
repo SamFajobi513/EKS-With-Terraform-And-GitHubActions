73ee23567e9d0a9a5ca1056dde7dd3dee0e8205d
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-08 23:32:50 +0100

    ec2-container-registry

--- i-am.tf
+++ i-am.tf
@@ -56,3 +56,9 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnly" {
+  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+}
