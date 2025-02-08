fb41dbe1cd64f164779773fa3d83dd19f29f9b2c
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-08 23:32:05 +0100

    eks-cni-policy

--- i-am.tf
+++ i-am.tf
@@ -50,3 +50,9 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKS_CNI_Policy" {
+  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+}
