d2975cc03e39604fb99c50fc0401cddd71ead544
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-08 23:41:48 +0100

    aws-ebs-driver-policy

--- i-am.tf
+++ i-am.tf
@@ -62,3 +62,9 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnl
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
+  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+}
