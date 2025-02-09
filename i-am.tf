253b420d042903864fb885268b17e28759046c45
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-09 17:54:48 +0100

    openIDconnect

--- i-am.tf
+++ i-am.tf
@@ -68,3 +68,26 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
+
+# OIDC
+resource "aws_iam_role" "eks_oidc" {
+  assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
+  name               = "eks-oidc"
+}
+
+resource "aws_iam_policy" "eks-oidc-policy" {
+  name = "test-policy"
+
+  policy = jsonencode({
+    Statement = [{
+      Action = [
+        "s3:ListAllMyBuckets",
+        "s3:GetBucketLocation",
+        "*"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+    Version = "2012-10-17"
+  })
+}
