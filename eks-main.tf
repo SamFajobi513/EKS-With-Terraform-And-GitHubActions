067d7695d93680078c412de64e1fa88ac394ba4a
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-10 23:17:35 +0100

    eks-oidc

--- eks-main.tf
+++ eks-main.tf
@@ -30,3 +30,12 @@ resource "aws_eks_cluster" "example" {
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
   ]
 }
+
+
+# OIDC Provider
+resource "aws_iam_openid_connect_provider" "eks-oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
+  url             = data.tls_certificate.eks-certificate.url
+}
+
