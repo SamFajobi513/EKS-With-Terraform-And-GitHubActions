87d364422f287d955365e3840f0cc13de10d21f5
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-10 23:10:04 +0100

    eks-update

--- eks-main.tf
+++ eks-main.tf
@@ -9,12 +9,12 @@ resource "aws_eks_cluster" "example" {
   version  = "1.31"
 
   vpc_config {
-    subnet_ids = [
-      aws_subnet.az1.id,
-      aws_subnet.az2.id,
-      aws_subnet.az3.id,
-    ]
+    subnet_ids              = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id, aws_subnet.private-subnet[2].id]
+    endpoint_private_access = var.endpoint-private-access
+    endpoint_public_access  = var.endpoint-public-access
+    security_group_ids      = [aws_security_group.eks-cluster-sg.id]
   }
+ 
 
   # Ensure that IAM Role permissions are created before and deleted
   # after EKS Cluster handling. Otherwise, EKS will not be able to
